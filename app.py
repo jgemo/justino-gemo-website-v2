aa2ea171ccd8d4c9c9fc3a9ec2cde6f46952215d
Justino Gemo <71757340+jgemo@users.noreply.github.com>
2023-03-20 16:48:00 +0000

    added jsonfy

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonfy
 
 app = Flask(__name__)
 
-JOBS = [
-  {
+JOBS = [{
     'id': 1,
     'title': 'Data Analyst',
     'location': ' Maputo, Mozambique',
     'salary': ' 120.000,00 MT'
-  },
-  {
+}, {
     'id': 2,
     'title': 'Frontend Engineer',
     'location': ' Remote',
-  },
-  {
+}, {
     'id': 3,
     'title': 'Backend Engineer',
     'location': ' JHB, South Africa',
     'salary': ' R 100.000,00'
-  },
-  {
+}, {
     'id': 4,
     'title': 'Data Scientist',
     'location': ' Lisbo, Portugal',
     'salary': ' Eur 130.000,00'
-  }
-]
+}]
+
 
 @app.route("/")
 def hello_justino():
-    return render_template('home.html',jobs=JOBS,company_name='Infordata')
+    return render_template('home.html', jobs=JOBS, company_name='Infordata')
+
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonfy(JOBS)
 
 
 if __name__ == "__main__":
