6145855891bf87b42d7b1689e34ec5c0d28a2378
Justino Gemo <71757340+jgemo@users.noreply.github.com>
2023-03-29 18:29:04 +0000

    Rendering JOBS api

--- database.py
+++ database.py
@@ -22,3 +22,14 @@ def load_jobs_from_db():
         for row in result.all():
             jobs.append(row._asdict())
     return jobs
+
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text(f"select * from jobs where id = {id}"))
+    rows = result.all()
+
+    if len(rows) == 0:
+        return None
+    else:
+        return rows[0]._asdict()
