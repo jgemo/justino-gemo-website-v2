c0c61f0bce6ff5380dfdc073170f739ed3cc03f1
Justino Gemo <71757340+jgemo@users.noreply.github.com>
2023-03-31 08:52:12 +0000

    Concluida paginas dynamicas

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_justino():
     jobs = load_jobs_from_db()
-    return render_template('home.html', jobs=jobs, company_name='Infordata')
+    return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
@@ -19,6 +19,10 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id)
+
+    if not job:
+        return "Not Found", 404
+
     return render_template('jobpage.html', job=job)
 
 
